9167d10064e7e2b56ed44ebdec6bb5349b709065
FleurD974 <fleur.dambreville@gmail.com>
2025-05-23 16:48:44 +0200

    adapt user form and enable profil viewing

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,8 +3,25 @@ from django import forms
 from accounts.models import Customer
 
 class UserForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput)
     
     class Meta:
         model = Customer
         fields = ["first_name", "last_name", "email", "password"]
+        widgets = {
+            'first_name': forms.TextInput(attrs={
+                'class': "form-control",
+                'style': 'max-width: 300px;'
+            }),
+            'last_name': forms.TextInput(attrs={
+                'class': "form-control",
+                'style': 'max-width: 300px;'
+            }),
+            'email': forms.EmailInput(attrs={
+                'class': "form-control",
+                'style': 'max-width: 300px;'
+            }),
+            'password': forms.PasswordInput(attrs={
+                'class':"form-control",
+                'style': 'max-width: 300px;'
+            })
+        }
